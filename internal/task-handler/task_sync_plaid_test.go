a6d2a047e001fd5b3dc0cb04fb63db07ce43f7e5
yoan <yoanyombapro@gmail.com>
2023-06-19 22:38:46 -0700

    feat: remove transactions
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- internal/task-handler/task_sync_plaid_test.go
+++ internal/task-handler/task_sync_plaid_test.go
@@ -2,12 +2,10 @@ package taskhandler
 
 import (
 	"context"
-	"reflect"
 	"testing"
 
 	"github.com/SimifiniiCTO/asynq"
 	"github.com/SimifiniiCTO/simfiny-financial-integration-service/internal/helper"
-	apiv1 "github.com/SimifiniiCTO/simfiny-financial-integration-service/pkg/generated/financial_integration_service_api/v1"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -58,58 +56,6 @@ func TestTaskHandler_RunSyncPlaidTransactionsTask(t *testing.T) {
 			tsk := tt.precondition(t, &tt.args)
 			if err := tt.th.RunSyncPlaidTransactionsTask(tt.args.ctx, tsk); (err != nil) != tt.wantErr {
 				t.Errorf("TaskHandler.RunSyncPlaidTransactionsTask() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func TestTaskHandler_pullReOcurringTransactions(t *testing.T) {
-	type args struct {
-		ctx         context.Context
-		userId      *uint64
-		linkId      *uint64
-		accessToken *string
-	}
-	tests := []struct {
-		name    string
-		th      *TaskHandler
-		args    args
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.th.pullReOcurringTransactions(tt.args.ctx, tt.args.userId, tt.args.linkId, tt.args.accessToken); (err != nil) != tt.wantErr {
-				t.Errorf("TaskHandler.pullReOcurringTransactions() error = %v, wantErr %v", err, tt.wantErr)
-			}
-		})
-	}
-}
-
-func TestTaskHandler_processSyncOperation(t *testing.T) {
-	type args struct {
-		ctx     context.Context
-		payload *SyncPlaidTaskPayload
-	}
-	tests := []struct {
-		name    string
-		th      *TaskHandler
-		args    args
-		want    []*apiv1.BankAccount
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := tt.th.processSyncOperation(tt.args.ctx, tt.args.payload)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("TaskHandler.processSyncOperation() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TaskHandler.processSyncOperation() = %v, want %v", got, tt.want)
 			}
 		})
 	}
