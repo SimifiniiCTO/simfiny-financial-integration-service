7313814f304484df0dc2b3b704343f6168fd5f52
yoan <yoanyombapro@gmail.com>
2023-07-25 22:52:49 -0400

    feat: checkpoint
    Signed-off-by: yoan <yoanyombapro@gmail.com>

--- pkg/grpc/grpc_analytics.go
+++ pkg/grpc/grpc_analytics.go
@@ -3,7 +3,6 @@ package grpc
 import (
 	"context"
 
-	clickhousedatabase "github.com/SimifiniiCTO/simfiny-financial-integration-service/internal/clickhouse-database"
 	proto "github.com/SimifiniiCTO/simfiny-financial-integration-service/pkg/generated/financial_integration_service_api/v1"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -11,47 +10,6 @@ import (
 
 func (s *Server) GetUserAccountBalanceHistory(context.Context, *proto.GetUserAccountBalanceHistoryRequest) (*proto.GetUserAccountBalanceHistoryResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetUserAccountBalanceHistory not implemented")
-}
-
-func (s *Server) GetMonthlyBalance(ctx context.Context, req *proto.GetMonthlyBalanceRequest) (*proto.GetMonthlyBalanceResponse, error) {
-	// perform validations
-	if req == nil {
-		return nil, status.Error(codes.InvalidArgument, "missing request")
-	}
-
-	if err := req.ValidateAll(); err != nil {
-		return nil, status.Error(codes.InvalidArgument, err.Error())
-	}
-
-	ctx, cancel := context.WithTimeout(ctx, s.config.RpcTimeout)
-	defer cancel()
-
-	// instrument operation
-	if s.instrumentation != nil {
-		txn := s.instrumentation.GetTraceFromContext(ctx)
-		span := s.instrumentation.StartSegment(txn, "grpc-get-monthly-balance")
-		defer span.End()
-	}
-
-	res, nextPageNumber, err := s.
-		clickhouseConn.
-		GetMonthlyBalance(
-			ctx,
-			&req.UserId,
-			&clickhousedatabase.BaseParams{
-				Month:        req.Month,
-				MerchantName: "",
-				PageSize:     uint64(req.PageSize),
-				PageNumber:   uint64(req.PageNumber),
-			})
-	if err != nil {
-		return nil, status.Error(codes.Internal, err.Error())
-	}
-
-	return &proto.GetMonthlyBalanceResponse{
-		MonthlyBalances: res,
-		NextPageNumber:  nextPageNumber,
-	}, nil
 }
 
 func (s *Server) GetMonthlyTotalQuantityBySecurityAndUser(context.Context, *proto.GetMonthlyTotalQuantityBySecurityAndUserRequest) (*proto.GetMonthlyTotalQuantityBySecurityAndUserResponse, error) {
