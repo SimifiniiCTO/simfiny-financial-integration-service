6ad4354a48c9099ba43a1d0b2457c101a1cdc39d
Simfinii_admin <82017432+SimifiniiCTO@users.noreply.github.com>
2023-09-06 22:47:17 -0400

    updating db migration

--- internal/migrations/20230723901290_categorymetricsfinancialsubprofilemv.up.sql
+++ internal/migrations/20230723901290_categorymetricsfinancialsubprofilemv.up.sql
@@ -1,5 +1,9 @@
-CREATE MATERIALIZED VIEW IF NOT EXISTS CategoryMetricsFinancialSubProfileMV
-TO CategoryMetricsFinancialSubProfile
+-- 1. Backup is an operational task, so it's not represented as an SQL command here.
+-- Ensure you've backed up your data!
+
+-- 2. Create a temporary materialized view with the updated logic.
+CREATE MATERIALIZED VIEW IF NOT EXISTS CategoryMetricsFinancialSubProfileMV_temp
+TO CategoryMetricsFinancialSubProfile_temp
 AS
 SELECT
     toYYYYMM(Time) as Month,
@@ -15,6 +19,14 @@ SELECT
 FROM
     TransactionInternal
 WHERE
-    Amount < 0 AND Sign = 1
+    Amount > 0 AND Sign = 1
 GROUP BY
     Month, PersonalFinanceCategoryPrimary, UserId;
+
+-- Populate the view (if necessary). Depending on how your DB and materialized views are set up, you might need to insert or refresh the data.
+
+-- 3. Once you have tested the data in the temporary materialized view and are satisfied, you can rename the old view and the new one.
+RENAME TABLE CategoryMetricsFinancialSubProfileMV TO CategoryMetricsFinancialSubProfileMV_backup;
+RENAME TABLE CategoryMetricsFinancialSubProfileMV_temp TO CategoryMetricsFinancialSubProfileMV;
+
+-- Additional steps like repointing to the original table and cleanup are operational and may vary based on your specific setup and needs. They aren't represented here.
